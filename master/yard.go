8ec2867bd0f51468eb17ec9db1d6eddbe56bb9a1
root <root@vs51068.ria.com>
2014-09-26 16:26:05 +0300

    add new data + modify port conn

--- master/yard.go
+++ master/yard.go
@@ -1,16 +1,102 @@
 package main
 
 import (
-    "fmt"
     "github.com/polluxx/yard/search"
+    "fmt"
+    "net/http"
+    "time"
+    "encoding/json"
+    "log"
+    
+    "regexp"
 )
 
+var validPath = regexp.MustCompile("^/(links|aggregate)/([a-zA-Z0-9]+)$")
+
 func main() {
-    Aggregate()
+    
+    //&http.HandleFunc("/", Handler)
+    
+    /*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	
+	switch path := r.URL.Path[len("//")]; path {
+	    case "links":
+		fmt.Fprintf(w, "Params %q - ", r.URL.Path[1:])
+	    case "aggregate":
+	    
+	    default:
+		fmt.Fprintf(w, "Route %q - not found!", html.EscapeString(r.URL.Path[1:]))
+	}
+	
+    })*/
+    
+    http.HandleFunc("/links/", mainHandler(linksHandler));
+    
+    s := &http.Server{
+	Addr:           ":8090",
+	//Handler:        Handle,
+	ReadTimeout:    10 * time.Second,
+	WriteTimeout:   10 * time.Second,
+	MaxHeaderBytes: 1 << 20,
+    }
+    
+    log.Fatal(s.ListenAndServe())
+
+    //Aggregate()
+}
+
+func mainHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+    
+    return func(w http.ResponseWriter, r *http.Request) {
+        mess := validPath.FindStringSubmatch(r.URL.Path)
+        
+        if mess == nil {
+    	    http.NotFound(w,r);
+            return
+        }
+        fn(w, r, mess[2])
+    }
+}
+
+func linksHandler(w http.ResponseWriter, r *http.Request, param string) {
+    switch param {
+	case "auto":
+	    GetProjectLinks(w, r, 1);
+	case "dom":
+	    GetProjectLinks(w, r, 3);
+	case "ria":
+	    GetProjectLinks(w, r, 2);
+	case "market":
+	    GetProjectLinks(w, r, 5);
+	default:
+	    http.NotFound(w, r)
+    }
+    
+}
+
+type Prof struct {
+    Name	string
+    Hobbies	[]string
+}
+
+func GetProjectLinks(w http.ResponseWriter, r *http.Request, project int) {
+
+    
+    data := search.Links(project)
+    
+    jsn, err := json.Marshal(data)
+    
+    if err != nil {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+	return
+    }
+    
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsn);
 }
 
 func Aggregate() {
-    items := search.LogSearch(1, "2014-Sep-09", 2, 10000);
+    items := search.LogSearch(2, "2014-Sep-01", 31, 10000);
     
     //search.Aggregate();
     
--- search/get.go
+++ search/get.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"time"
 	"strings"
+	"math/rand"
 )
 
 type Log struct {
@@ -16,7 +17,8 @@ type Log struct {
     city	string
     timedata	string
     day 	string
-    counter int
+    rangeItem	int
+    counter 	int
 }
 
 type Input struct {
@@ -30,62 +32,139 @@ var logged map[string] Log
 
 var filtered map[string] Log
 
-func Aggregate(results map[string]Log, item Log, key string) map[string]Log {
+func Aggregate(results map[string]Log, output chan map[string]Log) map[string]Log {
     
     //params := Input{"empty","empty","empty","group"};
     
+    tmpData := make(map[string]Log);
+    tmpData = <- output;
     
-    //for key, value := range item {
+    
+    for key, value := range tmpData {
 	//key := item.keyword;
 	_, exist := results[key];
 	if exist {
-	    item.counter = results[key].counter+1;
-	    item.rank = results[key].rank + item.rank
-	    results[key] = item;
+	    value.counter = results[key].counter+1;
+	    value.rank = results[key].rank + value.rank
+	    results[key] = value;
 	} else {
-	    results[key] = item;
+	    results[key] = value;
 	}
-    //}
+    }
     
     return results
 }
 
-func LogSearch(project int, from string, duration int, limit int) interface{} {
+func LogSearch(project int, from string, duration int, limit int) map[string]Log {
 
     
-    output := make(chan interface{})
+    //output := make(chan map[string]Log)
     
-    loc, _ := time.LoadLocation("Europe/Kiev");
+    //loc, _ := time.LoadLocation("Europe/Kiev");
     const longForm = "2006-Jan-02"
     t, _ := time.Parse(longForm, from)
     
-    t = t.In(loc);
+    //t = t.In(loc);
     
-    join := "00:00:00+0300"
+    join := "+0300"
     
     dates := strings.Fields(fmt.Sprintf("%s", t));
-    from = fmt.Sprintf("%v %s", dates[0], join);
+    from = fmt.Sprintf("%v %v%s", dates[0], dates[1], join);
+    
+    //filteredItems := make(map[string]Log)
+    
+    //output <- filteredItems
+    
+    results := make(map[string]Log)
     
     var valD int = 0;
-    go func() {
+    
+    duration *= 12;
+    
+    
+    
+    //go func() {
+    
+	allCount := 0;
 	for valD < duration {
 	    valD++;
-	    nextDate := t.AddDate(0, 0, valD);
+	    //nextDate := t.AddDate(0, 0, valD);
+	    nextDate := t.Add(time.Duration(120*valD)*time.Minute);
+	    
 	    datesTo := strings.Fields(fmt.Sprintf("%s", nextDate));
-	    to := fmt.Sprintf("%v %s", datesTo[0], join);
-	    GetLog(project, from, to, limit, output);
+	    to := fmt.Sprintf("%v %v%s", datesTo[0], datesTo[1], join);
+	    
+	    response := GetLog(project, from, to, limit);
+	    
+	    //fmt.Println(from, to)
+	    
+	    
 	    from = to;
+	    
+	    tmpData := make(map[string]Log);
+	    tmpData = <- response;
+	    
+	    //dataRes = Aggregate(dataRes, response);
+		
+	        for key, value := range tmpData {
+	    	    
+	    	    //key := item.keyword;
+	    	    _, exist := results[key];
+	    	    if exist {
+	    		
+	    		value.counter = results[key].counter+1
+	    		//value.range = value.rank - results[key].rank
+	    		value.rank += results[key].rank
+	    		results[key] = value
+	    	    } else {
+	    		allCount++;
+			results[key] = value
+	    	    }
+		}
+		
+	    
 	}
-	close(output);
-    }();
+	
+	
+	filterItems := make(map[string]Log)
+	for _,value := range results {
+	    
+	    if(value.counter > 1) {
+		value.rank /= value.counter;
+		value.counter = 1;
+		key := value.day
+		_, exist := filterItems[key]
+		if(exist) {
+		    
+		    value.rank += filterItems[key].rank;
+		    value.counter += filterItems[key].counter;
+		    filterItems[key] = value
+		} else {
+		    filterItems[key] = value
+		}
+		
+	    }
+	}
+	
+	for key, value := range filterItems {
+	    value.rank /= value.counter;
+	    value.rangeItem = allCount
+	    filterItems[key] = value;
+	}
+	
+	
+    //}();
     
     
-    return <- output;
+    return filterItems
+    
+    //return output
 }
 
 
-func GetLog(project int, from,to string, limit int, output chan interface{}) {
-
+func GetLog(project int, from,to string, limit int) <- chan map[string]Log {
+    output := make(chan map[string]Log)
+    
     location, _ := time.LoadLocation("Europe/Kiev")
 
     cluster := gocql.NewCluster("10.1.51.65","10.1.51.66")
@@ -106,37 +185,50 @@ func GetLog(project int, from,to string, limit int, output chan interface{}) {
     
     logged = make(map[string]Log)
     
-    for iter.Scan(&rank, &keyword, &cat, &subcat, &city, &timedata, &item) {
-	
-	time := timedata;
-	
-	t := timedata.In(location).Format(timeform)
-	day := time.In(location).Format(dayform)
-	
-	dayTime := strings.Split(day, "Z")
-	
-	valLog := Log{rank,keyword,cat,subcat,city, t, dayTime[0], 1}
-	
-	logged = Aggregate(logged, valLog, item);
-	
-    }
+    response := make(map[string]Log)
     
+    go func() {
     
-    filtered = make(map[string]Log)
-    
-    for _,value := range logged {
-    
-	if (value.counter > 1) {
-	    filtered = Aggregate(filtered, value, value.day)
+	for iter.Scan(&rank, &keyword, &cat, &subcat, &city, &timedata, &item) {
+	
+	    time := timedata;
+	
+	    t := timedata.In(location).Format(timeform)
+	    day := time.In(location).Format(dayform)
+	
+	    dayTime := strings.Split(day, "Z")
+	
+	    response[item] = Log{rank,keyword,cat,subcat,city, t, dayTime[0], 0, 1}
+	
+	//logged = Aggregate(logged, valLog, item);
+	
 	}
-    }
+	
+	output <- response
     
-    for keyIter,_ := range filtered {
+    }();
+    
+    
+    
+    //output := make(chan map[string]Log)
+    /*filtered := map[string]Log
+    counter := 0;
+    added := 0;
+    for _,value := range logged {
+	
+	if (value.counter > 1) {
+	    filtered = Aggregate(filtered, value, value.keyword)
+	    added++;
+	}
+	counter++;
+    }*/
+    
+    /*for keyIter,_ := range filtered {
 	insert := filtered[keyIter];
 	insert.rank = insert.rank / insert.counter;
 	
 	filtered[keyIter] = insert;
-    }
+    }*/
     
     if err := iter.Close(); err != nil {
 	log.Fatal(err)
@@ -144,5 +236,60 @@ func GetLog(project int, from,to string, limit int, output chan interface{}) {
 
     defer session.Close()
     
-    output <- filtered;
+    return output;
+}
+
+func Links(project int) map[string]string{
+    
+    cluster := gocql.NewCluster("10.1.51.65","10.1.51.66")
+    cluster.Keyspace = "avp"
+    session, _ := cluster.CreateSession()
+    
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+    
+    //item1 := ReturnRand(2, r)
+    item2 := ReturnRand(19, r)
+    //item1 := ReturnRand(8, r)
+    
+    //fmt.Print(item1, item2, item3);
+    
+    response := make(map[string]string)
+    tmpResp := make(map[string]string)
+    var keyword, link string
+    
+    query := fmt.Sprintf("select keyword, link from pure_data where project = %v and checked = 1 and priority > %v limit 30", project, item2)
+    
+    //fmt.Print(query);
+    iter := session.Query(query).Iter()
+    
+    for iter.Scan(&keyword, &link) {
+	tmpResp[keyword] = keyword
+	//fmt.Print(keyword);
+    }
+    
+    items := make([]string, len(tmpResp))
+    
+    i := 0;
+    for _, value := range tmpResp {
+	items[i] = value;
+	i++;
+    }
+    
+    for j := 0; j < 3; j++ {
+	rander := r.Intn(len(tmpResp))
+	key := items[rander]
+	response[key] = key
+    }
+    
+    
+    if err := iter.Close(); err != nil{
+	return response
+	log.Fatal(err);
+    }
+    
+    return response
+}
+
+func ReturnRand(volume int64, r *rand.Rand) string {
+    return fmt.Sprintf("%v%v", r.Int63n(volume), strings.Replace(fmt.Sprintf("%v", r.Float32()), "0", "", 1));
 }
